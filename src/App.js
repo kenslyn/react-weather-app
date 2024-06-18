6a53487226e1033fc5032880b0d5240f901854c0
Mackenzie Agnir <121129438+kenslyn@users.noreply.github.com>
2024-06-18 09:51:18 -0600

    Added an h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
         This project was coded by <a href="">Mackenzie Percy</a> and is{" "}
         <a href="https://github.com/kenslyn/react-weather-app" target="_blank">
