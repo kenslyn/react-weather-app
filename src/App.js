17cdcce3c1b8bc7bd46eb4c81d2b2b4ea85d8456
Mackenzie Agnir <121129438+kenslyn@users.noreply.github.com>
2024-06-18 10:20:34 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href="">Mackenzie Percy</a> and is{" "}
-        <a href="https://github.com/kenslyn/react-weather-app" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by <a href="">Mackenzie Percy</a> and is{" "}
+          <a
+            href="https://github.com/kenslyn/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
