2830a62b071d5bd1a6842302b5db34b4bea03aaa
Mackenzie Agnir <121129438+kenslyn@users.noreply.github.com>
2024-06-19 14:04:37 -0600

    Added the API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Calgary" />
         <footer>
           This project was coded by <a href="">Mackenzie Percy</a> and is{" "}
           <a
